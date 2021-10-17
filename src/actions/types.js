a9b371f4e0fa447ac031a4acd73e2d54b8ab1810
Jayant Raj <jayant.jnd@gmail.com>
2021-10-17 11:55:08 +0530

    Added backlogReducer.js

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,14 @@
+//Errors
 export const GET_ERRORS = "GET_ERRORS";
+
+//Projects
 export const GET_PROJECTS = "GET_PROJECTS";
 export const GET_PROJECT = "GET_PROJECT";
 export const DELETE_PROJECT = "DELETE_PROJECT";
+
+//Backlog
+export const GET_BACKLOG = "GET_BACKLOG";
+
+//Tasks
+export const GET_PROJECT_TASK = "GET_PROJECT_TASK";
+export const DELETE_PROJECT_TASK = "DELETE_PROJECT_TASK";
