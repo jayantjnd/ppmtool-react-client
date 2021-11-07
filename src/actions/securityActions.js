9ef2fee5032e252a7749873f6cd623754e16b296
Jayant Raj <jayant.jnd@gmail.com>
2021-11-07 21:59:29 +0530

    Security Reducer created and login action added

--- src/actions/securityActions.js
+++ src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setJWTInHeader from "../securityUtils/setJWTInHeader";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async (dispatch) => {
   try {
@@ -8,6 +10,35 @@ export const createNewUser = (newUser, history) => async (dispatch) => {
     dispatch({
       type: GET_ERRORS,
       payload: {},
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data,
+    });
+  }
+};
+
+export const login = (loginRequest) => async (dispatch) => {
+  try {
+    // post => Login request
+    const res = await axios.post("/api/users/login", loginRequest);
+
+    // extract token from res.data
+    const { token } = res.data;
+
+    // store the token in the localStorage
+    localStorage.setItem("jwtToken", token);
+
+    // set our token in the headers
+    setJWTInHeader(token);
+    // decode the token
+    const decodedToken = jwt_decode(token);
+
+    // dispatch to our security reducer
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decodedToken,
     });
   } catch (error) {
     dispatch({
